6a84cd781ce5d650e256ac1bd63c1c3bd1c06ae8
Mehreen Ramzan <mehreenramzan721@gmail.com>
2026-09-21 22:32:38 +0500

    commit

--- dist/index.js
+++ dist/index.js
@@ -2,29 +2,9 @@
 // let id: number = 5;
 // console.log("ID: ", id);
 Object.defineProperty(exports, "__esModule", { value: true });
-// basic types :
-let id = 5;
-let company = "Traversy Media";
-let isPublished = true;
-let x = "Hello";
-x = 9;
-console.log(x);
-// we will not get errors for this
-let age;
-age = 19;
-// array
-let ids = [1, 2, 3, 4, 5];
-// ids.push("ehue")// get error
-let other = [1, 2, "yids", true];
-// Tuple
-// in this we can specify proper types inside of the array
-let person = [1, "jsdj", true];
-// tuple array
-let employee;
-employee: [
-    [1, "jsd"],
-    [1, "jsd"],
-    [1, "jsd"],
-];
-// union
+const User1 = {
+    id: 12,
+    name: "H",
+};
+console.log(User1);
 //# sourceMappingURL=index.js.map
